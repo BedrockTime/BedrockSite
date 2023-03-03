6d76eab591afe22319512b5d1b04a91d4e16caf1
BedrockTime <116894396+BedrockTime@users.noreply.github.com>
2023-03-03 18:00:43 +0300

    commit 1
    YES

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return(
+        <header>
+            <div>
+                <span className="Logo">"Что-то для чего-то"</span>
+            </div>
+            
+            <div className="presentation"></div>
+        </header>
+    )
+}
